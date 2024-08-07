176c951c2524fb0d1ded27c03e3bf1f28206311f
sudenazercan <sudenaz162002@gmail.com>
2024-08-07 10:42:35 +0300

    test

--- test/src/main/java/com/example/test/config/SecurityConfig.java
+++ test/src/main/java/com/example/test/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 .decoder(jwtDecoder())
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter())
                         )
+
                 );
 
         return http.build();
